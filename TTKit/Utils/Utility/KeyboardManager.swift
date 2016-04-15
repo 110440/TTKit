6e5d55577f2e2a46c2239bd232488529a7df77f0
tanson <tanzongshan@hotmail.com>
2016-04-15 10:47:14 +0800

    KeyboradManager   fix

--- TTKit/Utils/Utility/KeyboardManager.swift
+++ TTKit/Utils/Utility/KeyboardManager.swift
@@ -18,15 +18,9 @@ public class KeyboardManager: NSObject {
     var isKeyboradShowNow:Bool = false
     
     let notificationCenter = NSNotificationCenter.defaultCenter()
- 
-    enum EvenType{
-        case Show
-        case Hide
-    }
     
     override init() {
         super.init()
-        //notificationCenter.addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(keyboardWillChangeFrame(_:)), name: UIKeyboardWillChangeFrameNotification, object: nil)
         notificationCenter.addObserver(self, selector:#selector(keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
     }
@@ -41,13 +35,7 @@ public class KeyboardManager: NSObject {
             return
         }
         
-        if isKeyboradShowNow == false{
-            isKeyboradShowNow = true
-            self.handleKeyboard(notification, even: .Show)
-        }else{
-            isKeyboradShowNow = false
-        }
-        
+        self.handleKeyboard(notification)
     }
     
     func keyboardWillHide(notification: NSNotification) {
@@ -55,30 +43,38 @@ public class KeyboardManager: NSObject {
         guard UIApplication.sharedApplication().applicationState != .Background else {
             return
         }
-        isKeyboradShowNow = false
-        self.handleKeyboard(notification, even: .Hide)
+        self.handleKeyboard(notification)
     }
 
-    private func handleKeyboard(notification: NSNotification,even:EvenType) {
+    private func handleKeyboard(notification: NSNotification) {
 
         if let userInfo = notification.userInfo {
-
+            
+            let screentSize = UIScreen.mainScreen().bounds.size
             let animationDuration: NSTimeInterval = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as! NSNumber).doubleValue
             let animationCurve = (userInfo[UIKeyboardAnimationCurveUserInfoKey] as! NSNumber).unsignedLongValue
-            //let frameBegin = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).CGRectValue()
+            let frameBegin = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).CGRectValue()
             let frameEnd = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
             
             let options = UIViewAnimationOptions(rawValue: animationCurve << 16 | UIViewAnimationOptions.BeginFromCurrentState.rawValue)
             
             UIView.animateWithDuration(animationDuration, delay: 0, options: options, animations: {
                 
-                switch even{
-                case .Show:
+                if frameBegin.minY >= screentSize.height {
+                    //弹出
+                    self.isKeyboradShowNow = true
                     self.animateWhenKeyboardAppear?(keyboardHeight:frameEnd.height)
-                case .Hide:
-                    self.animateWhenKeyboardDisappear?(keyboardHeight:frameEnd.height)
+                }else {
+                    if frameEnd.minY >= screentSize.height{
+                        //消失
+                        self.isKeyboradShowNow = false
+                        self.animateWhenKeyboardDisappear?(keyboardHeight:frameEnd.height)
+                    }else{
+                        //大小改变
+                        self.animateWhenKeyboardAppear?(keyboardHeight:frameEnd.height)
+                    }
+                    
                 }
-                
             }, completion: nil)
 
         }
